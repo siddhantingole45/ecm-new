5580a4bc543fcb134cfbb028ae8507bac1c1d446
Siddhant Ingole <siddhantingole10@gmail.com>
2024-12-23 17:38:21 +0530

    comment similar items

--- src/pages/BestSellers/BestSellers.jsx
+++ src/pages/BestSellers/BestSellers.jsx
@@ -1,5 +1,5 @@
 import Categories from "../../components/Categories/Categories";
-import SimilarItems from "../../components/SimilarItems/Similaritems";
+// import SimilarItems from "../../components/SimilarItems/Similaritems";
 import "./BestSellers.css";
 
 const BestSellers = () => {
@@ -23,7 +23,7 @@ const BestSellers = () => {
           </div>
         ))}
       </div>
-      <SimilarItems/>
+      {/* <SimilarItems/> */}
 
     </div>
   );
